a9e5bb6cd174bd5fa52713901da41b53c3df9af2
Andrew T <me@andrewtran-shi.dev>
2023-07-28 11:53:18 -0700

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/views/Main.jsx 	modified:   tailwind.config.js 	deleted:    tailwind.config.test.js

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -5,15 +5,30 @@ import React, { useState, useEffect } from "react";
 
 const Main = () => {
 	const [searchHistory, setSearchHistory] = useState([]);
+	const [darkMode, setDarkMode] = useState(false);
+
 	useEffect(() => {
 		const history = localStorage.getItem("searchHistory");
 		if (history) {
 			setSearchHistory(JSON.parse(history));
 		}
 	}, []);
+
+	useEffect(() => {
+		const body = document.querySelector("body");
+		if (darkMode) {
+			body.classList.add("dark-mode");
+		} else {
+			body.classList.remove("dark-mode");
+		}
+	}, [darkMode]);
+
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode);
+	};
+
 	return (
 		<div>
-
 			{searchHistory.length > 0 && (
 				<div>
 					<h2>Search history:</h2>
@@ -25,13 +40,17 @@ const Main = () => {
 				</div>
 			)}
 
+			<button className="btn-md glass accent" onClick={toggleDarkMode}>
+				{darkMode ? "Light Mode" : "Dark Mode"}
+			</button>
+
 			<Routes>
 				<Route path="/" element={<Search />} />
 				<Route path="/nutritison/:id" element={<NutritionTable />} />
 			</Routes>
-
 		</div>
 	);
 };
 
 export default Main;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
-		themes: ["dracula", "holloween"],
+		themes: ["dracula", "holloween", "cupcake"],
+		darkTheme: "holloween",
 	},
 };
